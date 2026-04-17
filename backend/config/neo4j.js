8895f0b67bc3fd733788f98512924d64dab631a5
Prajwalkiran1 <prajwalkiranofficial@gmail.com>
2026-04-17 20:55:51 +0530

    Updated Jenkinsfile and Neo4j config

--- backend/config/neo4j.js
+++ backend/config/neo4j.js
@@ -3,8 +3,11 @@ const neo4j = require("neo4j-driver");
 // Use the local Neo4j instance that you're viewing in Neo4j Browser,
 // so what the app writes is exactly what you see there.
 const driver = neo4j.driver(
-  "neo4j://localhost:7687",
-  neo4j.auth.basic("neo4j", "passwords")
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(
+    process.env.NEO4J_USER,
+    process.env.NEO4J_PASSWORD
+  )
 );
 
 module.exports = driver;
